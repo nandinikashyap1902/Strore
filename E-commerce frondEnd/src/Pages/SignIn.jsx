ea7397bce9445b64147b8fdd6bff0e1d835824b4
nandini <110680375+nandinikashyap1902@users.noreply.github.com>
2024-03-06 19:54:21 +0530

    some changes in layout

--- E-commerce frondEnd/src/Pages/SignIn.jsx
+++ E-commerce frondEnd/src/Pages/SignIn.jsx
@@ -3,8 +3,8 @@
 import Button from '@mui/material/Button';
 
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import Checkbox from '@mui/material/Checkbox';
 
 import Box from '@mui/material/Box';
 
@@ -15,7 +15,10 @@ import AccountCircleTwoToneIcon from '@mui/icons-material/AccountCircleTwoTone';
 import { Card } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import Navbar from './Navbar'
+
+
 export default function SignIn() {
+ 
   const navigate = useNavigate();
   const navigateHome =()=>{
     navigate('/')
@@ -29,7 +32,13 @@ export default function SignIn() {
     });
     navigate('/')
   };
-
+  const boxSX = {
+    "&:hover": {
+      background:'#436859',
+      color:'#FBFADA'
+    },
+  };
+  
   return (
     <>
     <Navbar>
@@ -46,32 +55,30 @@ export default function SignIn() {
         height:'350px'
       }}
     >
-      <Typography component="h1" variant="h5">
+      <Typography component="h1" variant="h5" color="#12372A">
         Sign in or Sign up
       </Typography>
-      <AccountCircleTwoToneIcon fontSize='large'/>
+      <AccountCircleTwoToneIcon fontSize='large'sx={{mt:3}}/>
       <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
         <TextField
           margin="normal"
           required
          fullWidth
           id="email"
-          label="Email Address"
+          label="Phone no."
           name="email"
           autoComplete="email"
           autoFocus
         />
-        <FormControlLabel
-          control={<Checkbox value="remember" color="primary" />}
-          label="Remember me"
-        />
+  
         <Button
           type="submit"
           
-          variant="contained"
-          sx={{ mt: 3, mb: 2 }}
-       onSubmit={()=>handleSubmit()}>
-          Sign In
+          variant="outlined"
+        
+          sx={boxSX}
+       onSubmit={()=>handleSubmit()} >
+          Countinue
         </Button>
       </Box>
     </Card>
